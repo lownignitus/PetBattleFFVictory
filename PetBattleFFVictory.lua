c8fc81a4a84ba7f9e87bbc46c437a6f4a92ae942
lownignitus <lownignitus@gmail.com>
2016-10-25 10:19:46 -0700

    TOC update for 7.1

--- PetBattleFFVictory.lua
+++ PetBattleFFVictory.lua
@@ -1,6 +1,6 @@
 -- Title: Pet Battle FF Victory
 -- Author: LownIgnitus
--- Version: 1.2.2
+-- Version: 1.2.3
 -- Desc: A simple addon to change the victory sound for pet battles
 -- Added Functionality to play fail sound and toggle its use
 
